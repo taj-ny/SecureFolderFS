ee0b1b3062eb72e636095c603a73a9915bdc6097
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-08-20 21:55:53 +0200

    Revert "Add Console.ReadKey() before Environment.Exit(0)"
    This reverts commit 6c7bc3603640233871b1783dd791b9bee6320ea6.

--- SecureFolderFS.Bruteforce/Program.cs
+++ SecureFolderFS.Bruteforce/Program.cs
@@ -20,7 +20,6 @@ Parallel.ForEach(File.ReadAllLines(passwordsFile), password =>
     {
         unlockRoutine.DeriveKeystore(new VaultPassword(password));
         Console.WriteLine($"\nFound password! \"{password}\"");
-        Console.ReadKey();
         Environment.Exit(0);
     }
     catch {}
